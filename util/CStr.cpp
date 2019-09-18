ca775915b5de4e17520e1ea11c54571eb98c017f
Joshua Weinstein <jweinstein@splunk.com>
2019-09-18 12:30:28 -0700

    growth

--- util/CStr.cpp
+++ util/CStr.cpp
@@ -1,5 +1,37 @@
 #include "CStr.h"
 
+#ifndef CSTR_DEFAULT_CAPACITY
+#define CSTR_DEFAULT_CAPACITY 25
+#endif
+
+#ifndef CSTR_DEFAULT_GROWTH
+#define CSTR_DEFAULT_GROWTH 3
+#endif
+
+CStr::CStr(): _cap(CSTR_DEFAULT_CAPACITY), 
+              _len(0),
+              _data(static_cast<char*>(calloc(1, CSTR_DEFAULT_CAPACITY)))
+{
+    
+}
+
+CStr::~CStr()
+{
+    free(_data);
+}
+
+void CStr::grow(size_t size)
+{
+    while((_cap - _len) > size) {
+#ifdef CSTR_GROW_POLICY_FACTOR
+    _cap *= CSTR_GROW_POLICY_FACTOR;
+#else
+    _cap *= CSTR_DEFAULT_GROWTH;
+#endif
+    _data = static_cast<char*>(realloc(_data, _cap));    
+    }
+}
+
 CStrIter::CStrIter(const char* string): _string(string)
 {
     
--- util/CStr.h
+++ util/CStr.h
@@ -4,10 +4,20 @@
 #include <cstdio>
 #include <cstdlib>
 
+
 class CStr {
 public:
-    const char* getData() const { return _data; }
+    CStr();
+    ~CStr();
+    const char& operator *() const { return *_data; }
     
+    const char* getData() const { return _data; }
+    size_t getLen() const { return _len; }
+    size_t getCap() const { return _cap; }
+  
+private:
+    void grow(size_t size);
+
 private:
     size_t _cap;
     size_t _len;
