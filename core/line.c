7e8800803340a4ff9cd83eb22d317ecd4791af68
Joshua Weinstein <jweinstein@splunk.com>
2019-09-22 11:13:40 -0700

    fix count issue

--- core/line.c
+++ core/line.c
@@ -13,11 +13,15 @@ int SpokenLine_read_file(SpokenLine* line, FILE* fp)
 
 int SpokenLine_readStr(SpokenLine* line, const char* string)
 {
+    char* writer = line->data;
     unsigned count = SPOKEN_LINE_MAX_LENGTH;
-    char * writer = line->data;
-    while(*string) {
-        *writer++ = *string++
+    while(*string && *string != '\n') {
+        if(!count) {
+            // line is longer than the limit.
+            return 0;
+        }
         count--;
+        *writer++ = *string++;
     }
     return 1;
 }
